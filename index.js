36f3d6528e63173602cd6a52f6001ede5cd6d94b
BrettSWeisberg <weisberg.brett@gmail.com>
2018-11-29 18:06:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
+
+// initiate our XHR request.
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
-function showRepositories() {
-  console.log(this.responseText);
-  let repoList = '<ul>';
-  for (var i = 0; i < this.responseText.length; i++) {
-    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-  }
-  repoList += '</ul>';
-  document.getElementById('repositories').innerHTML = repoList;
-}
+
+//Handling the XHR Response
 
 function showRepositories() {
   var repos = JSON.parse(this.responseText);
@@ -28,6 +24,16 @@ function showRepositories() {
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+
+//commit section 
 
 function getCommits(el) {
   const name = el.dataset.repo;
